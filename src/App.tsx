d193981ceffc1ef14a727b1a08d8717b1f059a68
Gibson Silali <gibsonsilali@gmail.com>
2022-08-27 00:39:30 +0300

    setup: added chakra-ui and chakra-ui icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+  Avatar,
+  Box,
+  Center,
+  Container,
+  Flex,
+  Icon,
+  Spacer,
+  Square,
+  Text,
+} from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxW={["100%", "80%"]} centerContent>
+        <Flex width={"100%"} color="white">
+          <Box w={["100%", "30%"]}>
+            <Flex justifyContent={"space-between"}>
+              <Avatar name="Gibson Silali"></Avatar>
+            </Flex>
+          </Box>
+
+          <Box flex="1" bg="tomato">
+            <Text>Box 3</Text>
+          </Box>
+        </Flex>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
