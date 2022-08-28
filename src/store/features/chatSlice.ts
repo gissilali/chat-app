a4e6919950c31f0cf4440d074b2d6c42bbd16746
Gibson Silali <gibsonsilali@gmail.com>
2022-08-29 01:33:41 +0300

    tab syncing is now succesful

--- src/store/features/chatSlice.ts
+++ src/store/features/chatSlice.ts
@@ -1,16 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { stat } from "fs";
 import { GroupChat } from "../../data/Interfaces";
 
+const initialMessages = localStorage.getItem("messages");
+const initialUsers = localStorage.getItem("users");
 const initialState: GroupChat = {
   id: "ac069757-5e0a-4437-8ad6-f0dc266803cc",
-  users: [],
-  messages: [],
+  users: initialUsers ? JSON.parse(initialUsers) : [],
+  messages: initialMessages ? JSON.parse(initialMessages) : [],
+  currentUser: null,
 };
 
 const chatSlice = createSlice({
-  name: "chart",
+  name: "chat",
   initialState,
-  reducers: {},
+  reducers: {
+    addCurrentUser: (state, { payload }) => {
+      state.currentUser = payload;
+    },
+    addUserToList: (state, { payload }) => {
+      state.currentUser = payload;
+      const users = [...state.users, payload];
+      state.users = users;
+      localStorage.setItem("users", JSON.stringify(state.users));
+    },
+    addMessage: (state, { payload }) => {
+      console.log("...adding Message");
+      const messages = [...state.messages, payload];
+      state.messages = messages;
+      localStorage.setItem("messages", JSON.stringify(messages));
+    },
+  },
 });
+
+export const { addCurrentUser, addMessage, addUserToList } = chatSlice.actions;
 
 export default chatSlice.reducer;
