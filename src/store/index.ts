f0b09021bf66500142e8cc59f4955ba4a803ca13
Gibson Silali <gibsonsilali@gmail.com>
2022-08-29 01:32:00 +0300

    setup react sync to enable syncing between tabs

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,22 @@
-import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  createStateSyncMiddleware,
+  initMessageListener,
+} from "redux-state-sync";
 import chatReducer from "./features/chatSlice";
+
+const config = {
+  blacklist: ["chat/addCurrentUser"],
+};
 
 const store = configureStore({
   reducer: {
     chat: chatReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(createStateSyncMiddleware(config)),
 });
+
+initMessageListener(store);
 
 export default store;
